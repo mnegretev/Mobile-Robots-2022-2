c666d6b523dc40a008dcd345f6ece4de3e6d07d3
Marvin The Paranoid Android <marvin@hitchhikersguidetothegalaxy.com>
2022-02-10 17:29:19 -0600

    codigo faltante

--- catkin_ws/src/students/scripts/practice00.py
+++ catkin_ws/src/students/scripts/practice00.py
@@ -22,6 +22,7 @@ def callback_scan(msg):
     # Do something to detect if there is an obstacle in front of the robot.
     # Set the 'obstacle_detected' variable with True or False, accordingly.
     #
+    obstacle_detected = msg.ranges[len(msg.ranges)//2] < 1.0 #Accedo a la lectura 
     return
 
 def main():
@@ -38,12 +39,12 @@ def main():
         # TODO:
         # Declare a Twist message and assign the appropiate speeds:
         msg = Twist() #ESto contiene un mienbro linear y angular
-        msg.linear.x = 0.1 
+        msg.linear.x = 0.1 if not obstacle_detected else 0.0
         # Move forward if there is no obstacle in front of the robot, and stop otherwise.
         # Use the 'obstacle_detected' variable to check if there is an obstacle. 
         # Publish the Twist message using the already declared publisher 'pub_cmd_vel'.
         #
-        pub_cmd_vel.publish(msg) #Verificar lo de cómo hacer un publicador y suscriptor
+        pub_cmd_vel.publish(msg) #Verificar lo de como hacer un publicador y suscriptor
         loop.sleep()
 
 
