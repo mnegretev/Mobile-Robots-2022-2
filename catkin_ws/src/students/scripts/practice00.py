77c1060ef8faab4c7d19ad700d4e6d960701642a
Marvin The Paranoid Android <marvin@hitchhikersguidetothegalaxy.com>
2022-02-24 12:15:29 -0600

    Práctica 1 resuelta

--- catkin_ws/src/students/scripts/practice00.py
+++ catkin_ws/src/students/scripts/practice00.py
@@ -22,6 +22,7 @@ def callback_scan(msg):
     # Do something to detect if there is an obstacle in front of the robot.
     # Set the 'obstacle_detected' variable with True or False, accordingly.
     #
+    obstacle_detected = msg.ranges[len(msg.ranges)//2] < 1.0
     return
 
 def main():
@@ -37,10 +38,13 @@ def main():
         #
         # TODO:
         # Declare a Twist message and assign the appropiate speeds:
+        msg = Twist()
+        msg.linear.x = 0.1 if not obstacle_detected else 0.0
         # Move forward if there is no obstacle in front of the robot, and stop otherwise.
         # Use the 'obstacle_detected' variable to check if there is an obstacle. 
         # Publish the Twist message using the already declared publisher 'pub_cmd_vel'.
         #
+        pub_cmd_vel.publish(msg)
         loop.sleep()
 
 
