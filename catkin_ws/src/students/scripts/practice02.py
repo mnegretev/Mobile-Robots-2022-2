1f5eaf9d7eae8926b1d7b5f9a1f504d56cf3f810
ItzelGitHub <biddygonzalez@gmail.com>
2022-03-01 13:06:11 -0600

    practica 2

--- catkin_ws/src/students/scripts/practice02.py
+++ catkin_ws/src/students/scripts/practice02.py
@@ -9,6 +9,7 @@
 # MODIFY ONLY THE SECTIONS MARKED WITH THE 'TODO' COMMENT
 #
 
+from cmath import cos
 import numpy
 import heapq
 import rospy
@@ -25,13 +26,232 @@ msg_path = Path()
 def a_star(start_r, start_c, goal_r, goal_c, grid_map, cost_map):
     #
     # TODO:
-    # Write the A* algorithm to find a path in an occupancy grid map given the start cell
-    # [start_r, start_c], the goal cell [goal_r, goal_c] and the map 'grid_map'.
-    # Return a set of points of the form [[start_r, start_c], [r1,c1], [r2,c2], ..., [goal_r, goal_c]]
-    # indicating the indices (cell coordinates) of the path cells.
-    # If path cannot be found, return an empty tuple []
+    # Escribe el algoritmo A* para encontrar una ruta en un mapa de ocupacion dada la celda de inicio
+    # [star_r, star_c], la celda meta [goal_r, goal_c] y el mapa 'grid map'
+    # Devuelve un conjunto de puntos de la forma [[start_r, start_c], [r1,c1], [r2,c2], ..., [goal_r, goal_c]]
+    # indicando los indices (coordenadas de celda) de las celdas de ruta.
+    # Si no se pudo encontrar la ruta, devuelve una tupla[] vacia
     #
+    n_start = [start_r, start_c] # Es 225,363
+    # Temp
+    #goal_r = 240
+    #goal_c = 390
+
+    n_goal = [goal_r, goal_c]
+    print("NODO DE INICIO: **************", n_start)
+    print("NODO DE meta: **************", n_goal)
+    # Vecinos
+    adjacents = []
+
+    # Distancia de nodo actual a vecinos
+    dist = 1
+
+    diagona_dist = dist*1.4142
+
+    # Lista abierta que guarda nodos por visitar y su costo de paso
+    open_list = []
+
+    # Lista cerrada que contiene nodos visitados 
+    closed_list = []#[[226,363], [224,363]]
+
+    # Registro de nodos hijos y padres
+    parents = {}
+
+    # g_cost
+    g_costs = numpy.full(grid_map.shape, float("inf"))
+    f_costs = numpy.full(grid_map.shape, float("inf"))   
+
+    # En la coordenada del nodo de inicio establecemos costo cero para g y f
+    g_costs[start_r, start_c] = 0
+
+    f_costs[start_r, start_c] = 0
+
+    print("g cost de nodo INICIO=  ", g_costs[start_r, start_c])
+
+    # Agrega nodo de inicio a lista abierta
+    open_list.append([[start_r, start_c], 0])
+    # 
+    path_found = False
+    
+    # Guarda la trayectoria mas corta
     path = []
+
+
+
+    rospy.loginfo("Inicializacion finalizada")
+
+    while open_list:
+        #rospy.loginfo("dentro de while")
+        
+        # Ordena open_list de acuerdo con el costo f mas bajo [[[x1,y1],f1], [[x2,y2],f2],...]
+        open_list.sort(key = lambda x: x[1])
+
+        #print("open list antes de pop", open_list)
+        # Extrae el primer elemento (coordenada)
+        n_actual = open_list.pop(0)[0]
+        #print("Saco el nodo con el menor f_cost: ",n_actual )
+
+        #print("openlist despues de pop", open_list)
+        #print("n_actual: ", n_actual)
+
+        # Nodo Actual***************************
+        r_actual = n_actual[0]
+        c_actual = n_actual[1]
+
+        # Agrega nodo actual a lista cerrada
+  #      if not n_actual in closed_list:
+        closed_list.append(n_actual)
+
+        #print("lista cerrada", closed_list)
+
+        # Si nodo actual es nodo meta salimos del ciclo
+        if n_actual == n_goal:
+            path_found = True
+            break
+
+        
+        # Vecinos de nodo actual
+
+        # Superior
+        if cost_map[r_actual-1, c_actual] <= 50:     # verifica si la celda no esta ocupada
+             # suma la distancia al vecino con costo en ese nodo
+            cost = dist + cost_map[r_actual-1, c_actual]   
+            adjacents.append([[r_actual-1, c_actual], cost])  # agrega elemento a lista vecinos
+            #print("registra nodo arriba, costo = ", cost)
+
+        # Izqierda
+        if cost_map[r_actual, c_actual-1] <= 50:
+            cost = dist + cost_map[r_actual, c_actual-1]
+            adjacents.append([[r_actual, c_actual-1], cost])
+            #print("registra nodo izq, costo = ", cost)
+
+        # Derecha
+        if cost_map[r_actual, c_actual+1] <= 50:
+            cost = dist + cost_map[r_actual, c_actual+1]
+            adjacents.append([[r_actual, c_actual+1], cost])
+            #print("registra nodo der, costo = ", cost)
+        
+        # Inferior
+        if cost_map[r_actual+1, c_actual] <= 50:
+            cost = dist + cost_map[r_actual+1, c_actual]
+            adjacents.append([[r_actual+1, c_actual], cost])
+            #print("registra nodo abajo, costo = ", cost)
+        
+        #print("lista de vecinos************************************************", adjacents)
+      
+        j = 0
+        #
+        # Analiza los nodos vecinos*************************************************
+        
+        for adjacent, cost in adjacents:
+            
+            #print("Analiza los vecinos de: ", n_actual )
+            #print("vecino num: ", j)
+            #print("coordenada de vecino en analisis: ", adjacent)
+            adj_r = adjacent[0]
+            adj_c = adjacent[1]
+
+            if adjacent in closed_list:     # si ya esta en lista cerrada lo omite
+                #print("Elemento repetido en vecinos: ",adjacent)
+                continue
+    
+        # Calcula g_cost
+        # g = g del padre + 1 si el paso es ortogonal o 1.4142 si el paso es diagonal
+        # mas el costo en esa celda
+            g_cost = g_costs[r_actual, c_actual] + cost  # Costo de paso + costo heredado
+        # Actualiza el g_gost de la celda del vecino en mapa g_costs
+            g_costs[adj_r, adj_c] = g_cost
+            #print("\n valor de costo del padre g_costs[r_actual, c_actual]******************", g_costs[r_actual, c_actual])
+            #print("\n")
+            #print("Valor de cost: ", cost)
+            #print("g_cost[adjacent]: ", g_costs[adj_r, adj_c])
+
+            h =  abs(goal_c-adj_r)+abs(goal_c-adj_c)# heuristica , distancia de manhattan
+            #print("\n Valor de h: ", h)
+            # Calcula f_cost
+            f_cost = g_cost + h
+  
+            in_open_list = False
+            index = 0
+      
+            for i in range(len(open_list)):
+                #print("i de 2o FOR: ", i)
+                # Verifica si nodo vecino esta en lista abierta
+                if open_list[i][0] == adjacent:
+                 #   print("nodo vecino ya esta en la lista")
+                    in_open_list = True
+                    index = i
+                    break
+
+            #adj_r = adjacent[0]
+            #adj_c = adjacent[1]
+            # CASO 1: nodo vecino ya esta en la lista abierta
+            if in_open_list:
+                if f_cost < f_costs[adj_r, adj_c]:  # Si costo actual es menor que costo anterior de nodo vecino
+                # actualiza el valor de f_cost
+                    f_costs[adj_r, adj_c] = f_cost
+                # actualiza la lista de parientes
+                    parents[adj_r, adj_c] = n_actual
+                # actualiza lista abieta
+                    open_list[i] = [adjacent, f_cost]
+                    #print("\nse actualizo costo f")
+
+            # Caso 2: nodo vecino no esta en la lista abierta
+            else:
+                # Actualiza f_cost
+                f_costs[adj_r, adj_c] = f_cost
+                 #f_costs[adjacent] = f_cost
+                #print("\ncosto F = ", f_cost)
+                #print("\n")
+                #print("\n valor de costo del padre g_costs[r_actual, c_actual]******************", g_costs[r_actual, c_actual])
+                #print("Valor de cost: ", cost)
+
+                #f_costs[adj_r, adj_c] = f_cost
+                # Registra en diccionario  al padre del vecino
+                parents[adj_r, adj_c] = n_actual
+                # aniade a lista abierta
+                open_list.append([adjacent, f_cost])
+               # print("Nodo vecino no estaba en lista abierta")
+
+            #print("FINAL DE DE FOR ANALIZA VECINOS vuelta num",j)
+            j += 1
+
+        #print("Lista vecinos final", adjacents)
+        #IMPLEMENTAR ALGORITMO PARA VACIAR LISTA DE VECINOS***********************************************
+        adjacents = []
+        #print("Lista vecinos vaciada", adjacents)
+        #print("\nNODO DE INICIO: ",start_r, start_c)
+        #print("\nNODO META: ", goal_r, goal_c)
+    
+
+    if not path_found:
+        rospy.logwarn('No se encontro una ruta')
+        return path
+
+    if path_found:
+        # Nodo debe ser una tupla
+        nodo = goal_r, goal_c
+        inicio = start_r, start_c
+        print("LLEGAMOS A LA PARTE DE ORDENAMIENTO DE RUTA**********************************************")
+        path.append(nodo)
+
+        #print("PATH con nodo META: ", path) #PATH es una lista
+
+        while nodo != inicio:
+    
+        # obtiene el siguiente nodo PERO ESTAMOS SACANDO UNA LISTA! ES NECESARIO CONVERTIRLA A TUPLA!!!************
+            nodo_list = parents[nodo]
+            
+            # Conversion a tupla
+            nodo = nodo_list[0], nodo_list[1]
+            path.append(nodo)
+            #print("PATH: ", path)
+
+        # Reconstruccion de la trayectoria
+        path = path[::-1]
+        print("si se encontro ruta")
+        print("RUTA OBTENIDA: ****************************", path)
+    
     return path
 
 def get_maps():
@@ -65,19 +285,21 @@ def callback_a_star(req):
     [zx, zy] = [s_map.info.origin.position.x, s_map.info.origin.position.y]
     print("Calculating path by A* from " + str([sx, sy])+" to "+str([gx, gy]))
     path = a_star(int((sy-zy)/res), int((sx-zx)/res), int((gy-zy)/res), int((gx-zx)/res), inflated_map, cost_map)
+
     msg_path.poses = []
     for [r,c] in path:
         msg_path.poses.append(PoseStamped(pose=Pose(position=Point(x=(c*res + zx), y=(r*res + zy)))))
     return GetPlanResponse(msg_path)
 
 def main():
-    print "PRACTICE 02 - " + NAME
+    print("PRACTICE 02 - " + NAME)
     rospy.init_node("practice02")
     rospy.wait_for_service('/static_map')
     rospy.Service('/path_planning/a_star_search'  , GetPlan, callback_a_star)
     pub_path = rospy.Publisher('/path_planning/a_star_path', Path, queue_size=10)
     loop = rospy.Rate(2)
     msg_path.header.frame_id = "map"
+    
     while not rospy.is_shutdown():
         pub_path.publish(msg_path)
         loop.sleep()
