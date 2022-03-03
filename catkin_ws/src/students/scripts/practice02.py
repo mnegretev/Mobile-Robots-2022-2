afad94d7951b1044bbf10d218c9f2900fc495e3b
Felipe-SG14 <felipe.solano.gos@gmail.com>
2022-03-03 04:08:39 -0600

    Practica 2 V1

--- catkin_ws/src/students/scripts/practice02.py
+++ catkin_ws/src/students/scripts/practice02.py
@@ -9,18 +9,24 @@
 # MODIFY ONLY THE SECTIONS MARKED WITH THE 'TODO' COMMENT
 #
 
+from operator import index
+from sqlite3 import Timestamp
 import numpy
 import heapq
 import rospy
 import math
+import time
 from geometry_msgs.msg import PoseStamped, Pose, Point
 from nav_msgs.msg import Path
 from nav_msgs.srv import *
 from collections import deque
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "SOLANO GONZALEZ FELIPE DE JESUS"
 
 msg_path = Path()
+
+def distancia(pInicial,pFinal):
+    return float(math.sqrt((pInicial[0]-pFinal[0])**2 + (pInicial[1]-pFinal[1])**2))
 
 def a_star(start_r, start_c, goal_r, goal_c, grid_map, cost_map):
     #
@@ -31,8 +37,81 @@ def a_star(start_r, start_c, goal_r, goal_c, grid_map, cost_map):
     # indicating the indices (cell coordinates) of the path cells.
     # If path cannot be found, return an empty tuple []
     #
-    path = []
-    return path
+
+    #Valores de g y f -> Inicializados en infinito
+    valores_g = numpy.full(grid_map.shape, float("inf"))
+    valores_f = numpy.full(grid_map.shape, float("inf"))
+    n_previo  = numpy.full((grid_map.shape[0], grid_map.shape[0], 2), -1)
+
+    #Definicion de nodos
+    nodoInicial = [start_r,start_c]
+    nodoMeta = [goal_r,goal_c]
+    nodosAdyacentes = [[1,0],[0,1],[-1,0],[0,-1],[1,1],[-1,1],[-1,-1],[1,-1]]
+
+    #Definicion de lista abierta y lista cerrada
+    openList = []
+    closedList = []
+
+    #Valores iniciales
+    valores_g[start_r,start_c] = 0
+    valores_f[start_r,start_c] = 0
+    openList.append(nodoInicial)
+
+    #Se inicializa
+    nodoActual = nodoInicial
+
+    while len(openList)>0 and nodoActual != nodoMeta:
+        #Se elige el nodo con menor valor de F
+        f_values = []
+        #Se busca todos los valores de F de la lista abierta
+        for i in range(0,len(openList)):
+            [x,y] = openList[i]
+            f_values.append(valores_f[x,y])
+        index_min_f = f_values.index(min(f_values)) 
+        #Se quita el de menor valore de F
+        nodoActual = openList.pop(index_min_f)
+        #Se inserta el nodo con menor valor
+        closedList.append(nodoActual)
+
+        #Recorriendo nodos adyacentes
+        for i in range(0,len(nodosAdyacentes)):
+            #Definiendo nodo actual y vecinos
+            [aX,aY] = nodoActual
+            [x,y]   = list(numpy.array(nodoActual)+numpy.array(nodosAdyacentes[i]))
+            #Variables auxiliares
+            if grid_map[x, y] != 0 or [x,y] in closedList:
+                continue
+            g = valores_g[aX,aY] + distancia([x,y],nodoMeta) + cost_map[x,y]
+            h = distancia([x,y],nodoMeta)
+            f = g+h
+            #Se da parametros a adyancentes que nos los tengan
+            if g < valores_g[x,y]:
+                valores_g[x,y] = g
+                valores_f[x,y] = f
+                n_previo [x,y] = [aX,aY]
+            
+            #Se agrega los nodos adyacentes posibles a la lista abierta
+            if (([x,y] not in openList) and ([x,y] not in closedList)):
+                openList.append([x,y])
+
+    #Comprobando la existencia de la ruta
+    if nodoActual != nodoMeta:
+        print("No es posible calcular la ruta")
+        return []
+
+    else:
+        print("Se calculo la ruta por el algoritmo de A*")
+        path = []
+        [x,y] = nodoActual
+        anterior = True
+        while(anterior != False):
+            path.insert(0,[x,y])
+            [x,y] = list(n_previo[x,y])
+            if ([x,y] == nodoInicial):
+                path.insert(0,[x,y])
+                anterior = False
+
+        return path
 
 def get_maps():
     print("Getting inflated and cost maps...")
@@ -71,7 +150,7 @@ def callback_a_star(req):
     return GetPlanResponse(msg_path)
 
 def main():
-    print "PRACTICE 02 - " + NAME
+    print ("PRACTICE 02 - " + NAME)
     rospy.init_node("practice02")
     rospy.wait_for_service('/static_map')
     rospy.Service('/path_planning/a_star_search'  , GetPlan, callback_a_star)
