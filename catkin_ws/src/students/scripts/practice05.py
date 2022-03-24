473ef5b28ebd4de87057cbd47f04e2c2d3ef9d85
Felipe-SG14 <felipe.solano.gos@gmail.com>
2022-03-24 14:22:08 -0600

    Practica 5

--- catkin_ws/src/students/scripts/practice05.py
+++ catkin_ws/src/students/scripts/practice05.py
@@ -9,6 +9,7 @@
 # Tune the constants alpha and beta to get a smooth movement. 
 #
 
+from numpy import angle
 import rospy
 import tf
 import math
@@ -19,7 +20,7 @@ from geometry_msgs.msg import Point
 from visualization_msgs.msg import Marker
 from sensor_msgs.msg import LaserScan
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "SOLANO GONZALEZ FELIPE DE JESUS"
 
 listener    = None
 pub_cmd_vel = None
@@ -27,20 +28,38 @@ pub_markers = None
 
 def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y):
     cmd_vel = Twist()
+    
     #
     # TODO:
     # Implement the control law given by:
-    #
-    # v = v_max*math.exp(-error_a*error_a/alpha)
-    # w = w_max*(2/(1 + math.exp(-error_a/beta)) - 1)
-    #
-    # where error_a is the angle error and
-    # v and w are the linear and angular speeds.
-    # v_max, w_max, alpha and beta, are design constants.
-    # Store the resulting v and w in the Twist message 'cmd_vel'
-    # and return it (check online documentation for the Twist message).
+    
+    # v and w are the linear and angular speeds taken as input signals
+    # and v_max, w_max, alpha and beta, are tunning constants.
+
+    v_max = 0.4
+    w_max = 0.5
+    alpha = 0.1
+    beta  = 0.1
+
+    # calculing angle error
+    # global error - robot angle
+    error_a = (math.atan2(goal_y-robot_y,goal_x-robot_x)) - robot_a
+
+    #Adjust angle error
+    #Domain [-pi,pi]
+
+    if error_a > math.pi:
+        error_a = error_a - 2*math.pi
+    elif error_a < (-math.pi):
+        error_a = error_a + 2*math.pi
+
+    v = v_max*math.exp(-error_a*error_a/alpha)
+    w = w_max*(2/(1 + math.exp(-error_a/beta)) - 1)
+    # Store the resulting v and w in the Twist message cmd_vel
+    # # and return it (check online documentation for the Twist message).
+    cmd_vel.linear.x  = v
+    cmd_vel.angular.z = w
     # Remember to keep error angle in the interval (-pi,pi]
-    #    
     return cmd_vel
 
 def attraction_force(robot_x, robot_y, goal_x, goal_y):
@@ -51,8 +70,18 @@ def attraction_force(robot_x, robot_y, goal_x, goal_y):
     # where force_x and force_y are the X and Y components
     # of the resulting attraction force w.r.t. map.
     #
+
+    #Constante
+    alfa = 1
+
+    #Calculando la magnitud 
+    magnitud = math.sqrt((robot_x-goal_x)**2+(robot_y-goal_y)**2)
+
+    #Calculando fuerza
+    force_x = alfa * (robot_x - goal_x) / magnitud
+    force_y = alfa * (robot_y - goal_y) / magnitud
     
-    return [0, 0]
+    return [force_x, force_y]
 
 def rejection_force(robot_x, robot_y, robot_a, laser_readings):
     #
@@ -66,12 +95,31 @@ def rejection_force(robot_x, robot_y, robot_a, laser_readings):
     # where force_x and force_y are the X and Y components
     # of the resulting rejection force w.r.t. map.
     #
-    return [0, 0]
+
+    #Defining variables
+    [force_x, force_y] = [0, 0]
+    mag = 0
+    beta = 4
+    d0 = 1
+
+    #Looking for the obstacles detected
+    for [distance,angle] in laser_readings:
+        if distance < d0:
+            mag = beta * math.sqrt(1/distance - 1/d0)
+        else:
+            mag = 0
+        force_x += mag * math.cos( angle + robot_a )
+        force_y += mag * math.sin( angle + robot_a ) 
+
+    n = len(laser_readings)
+    [force_x,force_y] = [ force_x/n , force_y/n ]
+
+    return [force_x, force_y]
 
 def callback_pot_fields_goal(msg):
     goal_x = msg.pose.position.x
     goal_y = msg.pose.position.y
-    print "Moving to goal point " + str([goal_x, goal_y]) + " by potential fields"    
+    print ("Moving to goal point " + str([goal_x, goal_y]) + " by potential fields")
     loop = rospy.Rate(20)
     global laser_readings
 
@@ -85,8 +133,12 @@ def callback_pot_fields_goal(msg):
     #
     # Set constant epsilon (0.5 is a good start)
     # Set tolerance  (0.1 is a good start)
+    epsilon = 0.5
+    tol = 0.1
     # Get robot position by calling robot_x, robot_y, robot_a = get_robot_pose(listener)
+    robot_x, robot_y, robot_a = get_robot_pose(listener)
     # Calculate distance to goal as math.sqrt((goal_x - robot_x)**2 + (goal_y - robot_y)**2)
+    d_to_goal = math.sqrt((goal_x - robot_x)**2 + (goal_y - robot_y)**2)
     # WHILE distance_to_goal_point > tolerance and not rospy.is_shutdown():
     #     Calculate attraction force Fa by calling [fax,fay]=attraction_force(robot_x,robot_y,goal_x, goal_y)
     #     Calculate rejection  force Fr by calling [frx,fry]=rejection_force (robot_x,robot_y,robot_a,laser_readings)
@@ -100,7 +152,30 @@ def callback_pot_fields_goal(msg):
     #     Wait a little bit of time by calling loop.sleep()
     #     Update robot position by calling robot_x, robot_y, robot_a = get_robot_pose(listener)
     #     Recalculate distance to goal position
+    while d_to_goal > tol and not rospy.is_shutdown():
+        #atracction force 
+        [fax,fay]=attraction_force(robot_x,robot_y,goal_x, goal_y)
+        #rejection force
+        [frx,fry]=rejection_force (robot_x,robot_y,robot_a,laser_readings)
+        #resulting force
+        [fresx,fresy] = [ fax + frx , fay + fry ]
+        #P = [px, py] = Pr - epsilon*F
+        [px , py] = [ robot_x - epsilon*fresx , robot_y - epsilon*fresy ]
+
+        msg_cmd_vel = calculate_control(robot_x, robot_y, robot_a, px, py)
+        pub_cmd_vel.publish(msg_cmd_vel)
+        draw_force_markers(robot_x, robot_y, fax, fay, frx, fry, fresx, fresy, pub_markers)
+        
+        loop.sleep()
+
+        robot_x, robot_y, robot_a = get_robot_pose(listener)
+        d_to_goal = math.sqrt((goal_x - robot_x)**2 + (goal_y - robot_y)**2)
+
     #  Publish a zero speed (to stop robot after reaching goal point)
+    stop_msg = Twist()
+    stop_msg.linear.x = 0
+    stop_msg.angular.z = 0
+    pub_cmd_vel.publish(stop_msg)
     print("Goal point reached")
 
 def get_robot_pose(listener):
@@ -134,7 +209,7 @@ def get_force_marker(robot_x, robot_y, force_x, force_y, color, id):
 
 def main():
     global listener, pub_cmd_vel, pub_markers
-    print "PRACTICE 05 - " + NAME
+    print ("PRACTICE 05 - " + NAME)
     rospy.init_node("practice05")
     rospy.Subscriber("/scan", LaserScan, callback_scan)
     rospy.Subscriber('/move_base_simple/goal', PoseStamped, callback_pot_fields_goal)
