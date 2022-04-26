1d248dd8e86b1f7120ceeb24477aaa9af0db2469
Felipe-SG14 <felipe.solano.gos@gmail.com>
2022-04-26 01:25:04 -0500

    Practica 7

--- catkin_ws/src/students/scripts/practice07.py
+++ catkin_ws/src/students/scripts/practice07.py
@@ -8,6 +8,7 @@
 # given a colored point cloud using color segmentation.
 #
 
+from ast import Pass
 import numpy
 import cv2
 import ros_numpy
@@ -18,27 +19,54 @@ from sensor_msgs.msg import PointCloud2
 from geometry_msgs.msg import PointStamped, Point
 from custom_msgs.srv import FindObject, FindObjectResponse
 
-NAME = "FULL_NAME"
+NAME = "SOLANO GONZALEZ FELIPE DE JESUS"
 
 def segment_by_color(img_bgr, points, obj_name):
     #
     # TODO:
     # - Assign lower and upper color limits according to the requested object:
+    upper_limit = [0]
+    lower_limit = [0]
     #   If obj_name == 'pringles': [25, 50, 50] - [35, 255, 255]
     #   otherwise                : [10,200, 50] - [20, 255, 255]
+    if obj_name == "pringles":
+        upper_limit = [35,255,255]
+        lower_limit = [25,50,50]
+    else:
+        upper_limit = [20,255,255]
+        lower_limit = [10,200,50]
     # - Change color space from RGB to HSV.
     #   Check online documentation for cv2.cvtColor function
+    img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
     # - Determine the pixels whose color is in the selected color range.
     #   Check online documentation for cv2.inRange
+    img_bin = cv2.inRange(img_hsv, numpy.array(lower_limit), numpy.array(upper_limit))
     # - Calculate the centroid of all pixels in the given color range (ball position).
     #   Check online documentation for cv2.findNonZero and cv2.mean
+    nz = cv2.findNonZero(img_bin)
+    mean = cv2.mean(nz)
     # - Calculate the centroid of the segmented region in the cartesian space
     #   using the point cloud 'points'. Use numpy array notation to process the point cloud data.
     #   Example: 'points[240,320][1]' gets the 'y' value of the point corresponding to
     #   the pixel in the center of the image.
     #
+    x, y, z = 0, 0, 0
+
+    for [[r,c]] in nz:
+        #  Verificacion de nulos
+        if math.isnan(points[r,c][0]) or math.isnan(points[r, c][1]) or math.isnan(points[r, c][2]):
+            pass
+        else:
+            x = x + points[r,c][0]
+            y = y + points[r,c][1]
+            z = z + points[r,c][2]
     
-    return [0,0,0,0,0]
+    #   Centroides
+    x = x / len(nz)
+    y = y / len(nz)
+    x = z / len(nz)
+
+    return [mean[0],mean[1],x,y,z]
 
 def callback_find_object(req):
     global pub_point, img_bgr
@@ -58,7 +86,7 @@ def callback_find_object(req):
 
 def main():
     global pub_point, img_bgr
-    print "PRACTICE 07 - " + NAME
+    print ("PRACTICE 07 - " + NAME)
     rospy.init_node("color_segmentation")
     rospy.Service("/vision/find_object", FindObject, callback_find_object)
     pub_point = rospy.Publisher('/detected_object', PointStamped, queue_size=10)
