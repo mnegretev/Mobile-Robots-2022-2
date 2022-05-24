8bbe5af2b68ac2378f1bc1472d52389805d8f562
Marvin The Paranoid Android <marvin@hitchhikersguidetothegalaxy.com>
2022-05-24 14:46:41 -0500

    proyecto final

---
+++ catkin_ws/src/students/scripts/final_project.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python
+#
+# MOBILE ROBOTS - FI-UNAM, 2022-2
+# FINAL PROJECT - SIMPLE SERVICE ROBOT
+# 
+# Instructions:
+# Write the code necessary to make the robot to perform the following possible commands:
+# * Robot take the <pringles|drink> to the <table|kitchen>
+# You can choose where the table and kitchen are located within the map.
+# Pringles and drink are the two objects on the table used in practice 07.
+# The Robot must recognize the orders using speech recognition.
+# Entering the command by text or similar way is not allowed.
+# The Robot must announce the progress of the action using speech synthesis,
+# for example: I'm going to grab..., I'm going to navigate to ..., I arrived to..., etc.
+# Publishers and suscribers to interact with the subsystems (navigation,
+# vision, manipulation, speech synthesis and recognition) are already declared. 
+#
+
+import rospy
+import tf
+import math
+import time
+from std_msgs.msg import String, Float32MultiArray, Float32, Bool
+from nav_msgs.msg import Path
+from nav_msgs.srv import GetPlan, GetPlanRequest
+from sensor_msgs.msg import PointCloud2
+from geometry_msgs.msg import Twist, PoseStamped, Pose, Point, PointStamped
+from sound_play.msg import SoundRequest
+from custom_msgs.srv import *
+
+NAME = "FULL_NAME"
+
+#
+# Global variable 'speech_recognized' contains the last recognized sentence
+#
+def callback_recognized_speech(msg):    #Reconoce la voz y publica un texto
+    global recognized_speech, new_task, executing_task
+    if executing_task:
+        return
+    new_task = True
+    recognized_speech = msg.data
+
+#
+# Global variable 'goal_reached' is set True when the last sent navigation goal is reached
+#
+def callback_goal_reached(msg):
+    global goal_reached
+    goal_reached = msg.data
+
+def parse_command(cmd):
+    obj = "pringles" if "PRINGLES" in cmd else "drink"
+    loc = [8.0,8.5] if "TABLE" in cmd else [3.22, 9.72]
+    return obj, loc
+
+#
+# This function sends the goal articular position to the left arm and sleeps 2 seconds
+# to allow the arm to reach the goal position. 
+#
+def move_left_arm(q1,q2,q3,q4,q5,q6,q7):
+    global pubLaGoalPose
+    msg = Float32MultiArray()
+    msg.data.append(q1)
+    msg.data.append(q2)
+    msg.data.append(q3)
+    msg.data.append(q4)
+    msg.data.append(q5)
+    msg.data.append(q6)
+    msg.data.append(q7)
+    pubLaGoalPose.publish(msg)
+    time.sleep(2.0)
+
+#
+# This function sends the goal angular position to the left gripper and sleeps 1 second
+# to allow the gripper to reach the goal angle. 
+#
+def move_left_gripper(q):
+    global pubLaGoalGrip
+    pubLaGoalGrip.publish(q)
+    time.sleep(1.0)
+
+#
+# This function sends the goal articular position to the right arm and sleeps 2 seconds
+# to allow the arm to reach the goal position. 
+#
+def move_right_arm(q1,q2,q3,q4,q5,q6,q7):
+    global pubRaGoalPose
+    msg = Float32MultiArray()
+    msg.data.append(q1)
+    msg.data.append(q2)
+    msg.data.append(q3)
+    msg.data.append(q4)
+    msg.data.append(q5)
+    msg.data.append(q6)
+    msg.data.append(q7)
+    pubRaGoalPose.publish(msg)
+    time.sleep(2.0)
+
+#
+# This function sends the goal angular position to the right gripper and sleeps 1 second
+# to allow the gripper to reach the goal angle. 
+#
+def move_right_gripper(q):
+    global pubRaGoalGrip
+    pubRaGoalGrip.publish(q)
+    time.sleep(1.0)
+
+#
+# This function sends the goal pan-tilt angles to the head and sleeps 1 second
+# to allow the head to reach the goal position. 
+#
+def move_head(pan, tilt):  #Mueve la cabeza para ver al escritorio
+    global pubHdGoalPose
+    msg = Float32MultiArray()
+    msg.data.append(pan)
+    msg.data.append(tilt)
+    pubHdGoalPose.publish(msg)
+    time.sleep(1.0)
+
+#
+# This function sends a linear and angular speed to the mobile base to perform
+# low-level movements. The mobile base will move at the given linear-angular speeds
+# during a time given by 't'
+#
+def move_base(linear, angular, t):
+    global pubCmdVel
+    cmd = Twist()
+    cmd.linear.x = linear
+    cmd.angular.z = angular
+    pubCmdVel.publish(cmd)
+    time.sleep(t)
+    pubCmdVel.publish(Twist())
+
+#
+# This function publishes a global goal position. This topic is subscribed by
+# pratice04 and performs path planning and tracking.
+#
+def go_to_goal_pose(goal_x, goal_y): #Publica un punto
+    global pubGoalPose
+    goal_pose = PoseStamped()
+    goal_pose.pose.orientation.w = 1.0
+    goal_pose.pose.position.x = goal_x
+    goal_pose.pose.position.y = goal_y
+    pubGoalPose.publish(goal_pose)
+
+#
+# This function sends a text to be synthetized.
+#
+def say(text):    #Recibe un texto y lo publica 
+    global pubSay
+    msg = SoundRequest()
+    msg.sound   = -3
+    msg.command = 1
+    msg.volume  = 1.0
+    msg.arg2    = "voice_kal_diphone"
+    msg.arg = text
+    pubSay.publish(msg)
+
+def main():
+    global new_task, recognized_speech, executing_task, goal_reached
+    global pubLaGoalPose, pubRaGoalPose, pubHdGoalPose, pubLaGoalGrip, pubRaGoalGrip
+    global pubGoalPose, pubCmdVel, pubSay
+    print("FINAL PROJECT - " + NAME)
+    rospy.init_node("final_exercise")
+    rospy.Subscriber('/recognized', String, callback_recognized_speech)
+    rospy.Subscriber('/navigation/goal_reached', Bool, callback_goal_reached)
+    pubGoalPose = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+    pubCmdVel   = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    pubSay      = rospy.Publisher('/robotsound', SoundRequest, queue_size=10)
+    pubLaGoalPose = rospy.Publisher("/hardware/left_arm/goal_pose" , Float32MultiArray, queue_size=10);
+    pubRaGoalPose = rospy.Publisher("/hardware/right_arm/goal_pose", Float32MultiArray, queue_size=10);
+    pubHdGoalPose = rospy.Publisher("/hardware/head/goal_pose"     , Float32MultiArray, queue_size=10);
+    pubLaGoalGrip = rospy.Publisher("/hardware/left_arm/goal_gripper" , Float32, queue_size=10);
+    pubRaGoalGrip = rospy.Publisher("/hardware/right_arm/goal_gripper", Float32, queue_size=10);
+    listener = tf.TransformListener()
+    loop = rospy.Rate(10)
+    print("Waiting for services...")
+    rospy.wait_for_service('/manipulation/la_inverse_kinematics')
+    rospy.wait_for_service('/vision/find_object')
+    print("Services are now available.")
+    clt_la_inverse_kin = rospy.ServiceProxy("/manipulation/la_inverse_kinematics", InverseKinematics)
+    clt_ra_inverse_kin = rospy.ServiceProxy("/manipulation/ra_inverse_kinematics", InverseKinematics)
+    clt_find_object = rospy.ServiceProxy("/vision/find_object", FindObject)
+
+    new_task = False
+    executing_task = False
+    recognized_speech = ""
+    goal_reached = False
+
+    current_state = "SM_INIT"
+    requested_object   = ""
+    requested_location = [0,0]
+
+    #
+    # FINAL PROJECT
+    # 
+    #
+    
+    
+    while not rospy.is_shutdown():
+        if current_state == "SM_INIT":
+		current_state ="HELLO"
+	if current_state == "HELLO":
+		say("Estoy vivo")
+		current_state = "SM_INIT"
+        loop.sleep()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
+    
+
