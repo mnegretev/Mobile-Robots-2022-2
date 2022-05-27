f4863edea899c26759507e9ed6d799baaa166435
Marvin The Paranoid Android <marvin@hitchhikersguidetothegalaxy.com>
2022-05-27 10:37:47 -0500

    Pracica7

--- catkin_ws/src/students/scripts/practice07.py
+++ catkin_ws/src/students/scripts/practice07.py
@@ -18,7 +18,7 @@ from sensor_msgs.msg import PointCloud2
 from geometry_msgs.msg import PointStamped, Point
 from custom_msgs.srv import FindObject, FindObjectResponse
 
-NAME = "FULL_NAME"
+NAME = "Gonzalez Pastor Liliana"
 
 def segment_by_color(img_bgr, points, obj_name):
     #
@@ -37,8 +37,28 @@ def segment_by_color(img_bgr, points, obj_name):
     #   Example: 'points[240,320][1]' gets the 'y' value of the point corresponding to
     #   the pixel in the center of the image.
     #
-    
-    return [0,0,0,0,0]
+    lower_limit = [0]
+    upper_limit = [0]
+        if obj_name == 'pringles': 
+    	lower_limit = [25, 50, 50]     # Minimum value of hue, saturation and value of pringles.
+        upper_limit  = [35, 255, 255]  # Maximum value of hue, saturation and value of pringles.
+    else: 
+        lower_limit= [10,200, 50]     # Minimum value of hue, saturation and value of soda.
+	upper_limit  = [20, 255, 255]  # Maximum value of hue, saturation and value of soda.
+    img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
+    img_bin = cv2.inRange(img_hsv, lower_limit, upper_limit)
+    idx = cv2.findNonZero(img_bin)
+    [img_x, img_y, a, b] = cv2.mean(idx)
+    [x,y,z] = [0,0,0]
+    counter = 0
+    for [[c, r]] in idx:
+        xt, yt, zt = points[r,c][0], points[r,c][1], points[r,c][2]
+        if math.isnan(xt) or math.isnan(yt) or math.isnan(zt):
+            continue
+        [x,y,z,counter] = [x+xt, y+yt, z+zt, counter+1]
+    [x,y,z] = [x/counter, y/counter, z/counter] if counter > 0 else [0,0,0]
+    return [img_x, img_y, x,y,z]
+   
 
 def callback_find_object(req):
     global pub_point, img_bgr
